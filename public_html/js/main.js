a2ae104758ddf1405ac3d3eeb0d71a611436e69d
Shreyas <seekshreyas@gmail.com>
2013-09-19 19:37:15 -0700

    started working with javascript

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,36 @@
+var MADLIB = MADLIB || {}
 
+MADLIB = (function(){
+
+
+	var init = function(){
+		console.log("page loaded");
+		parseLyrics()
+	}
+
+
+	function parseLyrics(){
+		//to show prettier lyrics
+		//I love this song. it deserves proper respect of being 
+		//displayed right :)
+		jQuery('#wrapper-madlib p').each(function(){
+
+			var lyric = jQuery(this).text();
+
+			newlyric = lyric.replace(",", "<br>");
+
+			jQuery(this).html(newlyric);
+		});
+	}
+
+
+
+	return {
+		'init' : init
+	}
+})()
+
+
+jQuery(document).ready(function(){
+	MADLIB.init()
+});
