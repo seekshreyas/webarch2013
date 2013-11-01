6e117f037dc46cc60529b50345b7d3e868bb1f51
Shreyas <seekshreyas@gmail.com>
2013-10-31 20:14:50 -0700

    regex testing

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -2,11 +2,7 @@ var MADLIB = MADLIB || {};
 
 MADLIB = (function(){
 
-	// regex reference link:
-	// http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
-
-	var re = '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/';
-
+	
 
 	var init = function(){
 		console.log("page loaded");
@@ -23,7 +19,19 @@ MADLIB = (function(){
 
 			lurl = jQuery('#field-long').val();
 
+
+			// regex reference link:
+			// http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
+
+			var re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/
+
 			console.log(lurl);
+
+			if (re.test(lurl)){
+				jQuery(this).submit();
+			}else{
+				alert("long url not in correct format");
+			}
 			
 
 
